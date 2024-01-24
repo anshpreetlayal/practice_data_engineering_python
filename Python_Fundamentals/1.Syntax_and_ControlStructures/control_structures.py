e032b5078131d341a4740c79af9600f95872f8f0
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-01-23 19:13:21 -0500

    practicing control flow

--- Python_Fundamentals/1.Syntax_and_ControlStructures/control_structures.py
+++ Python_Fundamentals/1.Syntax_and_ControlStructures/control_structures.py
@@ -1,7 +1,13 @@
-# =======================================
-#        Understanding Control flow in Python
-# =======================================
+# Python Fundamentals for Data Engineering
+# control_flow.py: Covers control flow structures and conditional statements.
 
-# ---------------------------------------------
-# Introduction to Conditional Statments
-# ---------------------------------------------
+# ----------------------------------------------
+# Control Structures: Conditional Statements
+# ----------------------------------------------
+
+# Sample data for conditional statements
+temperature = 30
+is_raining = False
+is_weekend = True
+
+# Basic if statement
