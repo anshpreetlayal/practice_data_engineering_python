1e46cb1e6f513d389684e5126b2f6b1e840196b2
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-02-02 13:07:28 -0500

    oop

--- Advanced_python_concepts_in_DE/OOP/oop_basics.py
+++ Advanced_python_concepts_in_DE/OOP/oop_basics.py
@@ -34,4 +34,26 @@ class FoodItem(ABC):
         Returns: None
         """
         print(f"{self.name} - Calories: {self.calories} kcal")
+        
+# ----------------------------------------------
+# Dessert Class Definition (Inheritance)
+# ----------------------------------------------
 
+class Dessert(FoodItem):
+    """
+    Dessert class (inherits from FoodItem): Represents a dessert.
+
+    Additional Attributes:
+    - flavor (str): The flavor of the dessert.
+    """
+    def __init__(self, name, flavor, calories):
+        """
+        Constructor method (init) for Dessert class.
+
+        Parameters:
+        - name (str): The name of the dessert.
+        - flavor (str): The flavor of the dessert.
+        - calories (int): The initial calorie content of the dessert.
+        """
+        super().__init__(name, calories)
+        self.flavor = flavor
