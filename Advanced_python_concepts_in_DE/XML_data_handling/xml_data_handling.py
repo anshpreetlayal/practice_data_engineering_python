e0da297e733bbbc1ac745fbb650212624bca425b
Anshpreet Kaur <anshpreetlayal@outlook.com>
2024-02-06 13:30:16 -0500

    xml operations

--- Advanced_python_concepts_in_DE/XML_data_handling/xml_data_handling.py
+++ Advanced_python_concepts_in_DE/XML_data_handling/xml_data_handling.py
@@ -1,5 +1,33 @@
-'''
-Working with XML Data:
+# =======================================
+#      Working with XML Data
+# =======================================
 
-Learn the basics of XML and how to parse and manipulate XML data in Python.
+# Python Fundamentals for Data Engineering
+# xml_processing.py: Demonstrates parsing and manipulating XML data.
+
+# Importing necessary libraries
+import xml.etree.ElementTree as ET
+
+# Sample XML data
+xml_data = '''
+<bookstore>
+  <book category="cooking">
+    <title lang="en">Everyday Italian</title>
+    <author>Giada De Laurentiis</author>
+    <year>2005</year>
+    <price>30.00</price>
+  </book>
+  <book category="children">
+    <title lang="en">Harry Potter</title>
+    <author>J.K. Rowling</author>
+    <year>2003</year>
+    <price>29.99</price>
+  </book>
+  <book category="web">
+    <title lang="en">Learning XML</title>
+    <author>Erik T. Ray</author>
+    <year>2003</year>
+    <price>39.95</price>
+  </book>
+</bookstore>
 '''
